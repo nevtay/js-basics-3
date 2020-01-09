bdc4f3aab07dba7717be2b18a299c7bf93ed84f2
nevtay <nevintay@gmail.com>
2020-01-09 15:57:50 +0800

    attempted by nevin

--- exercise-4.js
+++ exercise-4.js
@@ -1,43 +1,51 @@
 // 1. Create an array `names` with at least 3 elements: `'david'`, `'gordon'`, and
 //    `'sahil'` and log it to the console
-
+let arr = ['david', 'gordon', 'sahil'];
 
 // 2. Log the value of the third element in `names` to the console
 
+console.log(arr[3]);
 
 // 3. Log the value of the `names` array's `length` property to the console
-
+console.log(arr.length);
 
 // 4. Set the value of the `length` property to `1`
-
+arr.length = 1;
 
 // 6. Once again, log the value of the `names` array's `length` property to the
 //    console. Did it change?
-
+console.log(arr.length);
 
 // 7. Log the entire `names` array to the console again
-
+console.log(arr.map(x => x));
 
 // 8. Add a few more elements to the array using `push`
-
+let newArr = ['Tim', 'Sally', 'Jerome'];
+newArr.map(name => arr.push(name));
 
 // 9. Log the last element of the array to the console
-
+console.log(arr[arr.length - 1]);
 
 // 10. Log the second last element of the array to the console
-
+console.log(arr[arr.length - 2])
 
 // 11. Change the `textContent` property of the _second_ `p` tag on the page to
 //    `"JavaScript was here!"`
+let p = document.querySelectorAll('p');
+p[1].innerHTML = 'JavaScript was here!';
 
 
 // 12. Convert the `names` array to a string, with each element separated by a
 //    '<br>'. Store it in a new variable called `namesString` e.g. if `names` is
 //    `['harry', 'ron', 'dean']` then `namesString` will be `"harry<br>ron<br>dean"`
+const namesString = arr.join('<br>');
+console.log('joined array is ', namesString, typeof namesString);
 
 
 // 13. Change the `innerHTML` property of the _first_ `p` tag on the page to
 //    `namesString`. What happens when you use `textContent` instead of
 //    `innerHTML`?
+p[0].innerHTML = namesString;
+// p[0].innerText = namesString;
 
 
--- exercise-5.js
+++ exercise-5.js
@@ -1,17 +1,56 @@
 // 1. Create a new array `shoppingList` with a few string elements
+const shoppingList = ['item 1', 'item 2', 'item 3']
 
+// 2a. Using `for loop`, log every element of the `shoppingList` array to the console
+for (let i = 0; i < shoppingList.length; i -= -1) {
+    console.log(shoppingList[i]);
+}
 
-// 2. Using `forEach`, log every element of the `shoppingList` array to the console
-
+// 2b. Using `forEach`, log every element of the `shoppingList` array to the console
+shoppingList.forEach(item => {
+    console.log(`${item}`);
+})
 
 // 3. For each element in the `shoppingList` array, create an `<li>` node and
 //    append it to the `<ul>` element on the page (use `document.createElement`
 //    and `.appendChild`)
+let list = document.querySelector('ul');
+shoppingList.map((item,idx) => {
+    if (idx === shoppingList.length - 1) {
+        list.innerHTML += `${item}`;
+    } else {
+        list.innerHTML += `${item} <hr>`;
+    }
+    list.style.background = 'gold';
+    list.style.padding = '30px 20px';
+})
 
 
 // 4. Create a new array `numbers` with a few numeric elements
-
+let arr2 = [1, 2, 3];
 
 // 5. Using `map` on the `numbers` array, create a new array called
 //    `squareNumbers` where each element is the square of the corresponding
 //    element in the `numbers` array
+const sqNum = arr2.map(num => num ** 2);
+console.log(sqNum);
+// 5a. Using `for loop` on the `numbers` array, create a new array called
+//    `squareNumbers` where each element is the square of the corresponding
+//    element in the `numbers` array
+const sqNum2 = [];
+for (i = 0; i < arr2.length; i++) {
+    sqNum2.push(arr2[i] * arr2[i]);
+}
+console.log(sqNum2);
+
+// 5b. Using `forEach` on the `numbers` array, create a new array called
+//    `squareNumbers` where each element is the square of the corresponding
+//    element in the `numbers` array
+const sqNum3 = [];
+arr2.forEach(num => {
+    sqNum3.push(num * num);
+})
+
+console.log(sqNum3);
+
+console.log('we\'re done')
